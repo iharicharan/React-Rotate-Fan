5573df560d27bc63e98c6dc1083a17c16f1d2cbe
iharicharan <xprogrammer.me@gmail.com>
2020-12-04 21:56:36 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import rotate from './logo.png'
+import './App.scss'
+export default function fan(){
+  function press1(){
+    let x= document.getElementsByClassName('section-img')[0]
+   x.classList.toggle('section-imgone')
+   x.classList.remove("section-imgtwo")
+   x.classList.remove("section-imgthree")
+      
+   }
+   function press2(){
+     let x= document.getElementsByClassName('section-img')[0]
+     x.classList.toggle('section-imgtwo')
+     x.classList.remove("section-imgone")
+     x.classList.remove("section-imgthree")
+     
+   
+    }
+    function press3(){
+     let x= document.getElementsByClassName('section-img')[0]
+      x.classList.toggle('section-imgthree')
+     x.classList.remove("section-imgtwo")
+    x.classList.remove("section-imgone")
+     
+    }
+    function press0(){
+     let x= document.getElementsByClassName('section-img')[0]
+    x.classList.remove("section-imgthree")
+     x.classList.remove("section-imgtwo")
+     x.classList.remove("section-imgone")
+     
+    }
+    
+  return(
+    <>
+    <div style={{marginTop:'3%'}}>
+    <center>
+    <a href="https://twitter.com/iharicharan?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @iharicharan</a>
+    </center>
     </div>
+  <section className='section'>  
+   <img src={rotate} alt='' className='section-img'/>
+  </section>
+  <article>
+    <div className='div'></div>
+  </article>
+    <article className='article'>
+<div className='article-div'> 
+<button onClick={press0}>OFF</button>
+<button onClick={press1}>1</button>
+<button onClick={press2}>2</button>
+<button onClick={press3}>3</button>
+</div>
+</article>
+
+   
+    </>
   );
 }
-
-export default App;
